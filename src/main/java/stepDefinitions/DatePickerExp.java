af0c6c56015078f6760466f2f4c1c34e4b518e19
hzkSJSU <78756164+hzkSJSU@users.noreply.github.com>
2023-12-04 20:10:50 -0800

    Date Picker Class modified

--- src/main/java/stepDefinitions/DatePickerExp.java
+++ src/main/java/stepDefinitions/DatePickerExp.java
@@ -6,6 +6,7 @@ public class DatePickerExp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Git and Github");
+		System.out.println("Date Picker Example...");
 	}
 
 }
